6b5f572de9bd509b01044d8f8e772a69c826976b
meairey <mea2194@columbia.edu>
2018-03-23 16:57:45 -0400

    removed unwanted folder

--- Notebooks/record/record/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-
-from record.record import Record
--- Notebooks/record/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name="record",
-    version="0.1",
-    packages=find_packages(),
-    author = "Montana Airey",
-    license= "GPLv3",
-    classifiers=["Programming Language :: Python :: 3"],
-)
